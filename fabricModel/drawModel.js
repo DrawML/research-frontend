36c7b711fa9e612b411b5c497d584351453b19f2
koohanmo <rngksah@naver.com>
2016-09-06 18:00:02 +0900

    add NN Add/Delete Layerm choose Activation

--- fabricModel/drawModel.js
+++ fabricModel/drawModel.js
@@ -412,12 +412,24 @@ function Layer(id,activation,input,output){
 
     this.setId = function(id){
         this.id=id;
-        this.fabricModel.getObjects()[1].setText('Layer' +id);
+        this.fabricModel.getObjects()[1].setText('Layer ' +id);
         // this.model.item(1).set({
         //     left : -this.model.item(1).getWidth()/2,
         //     top : -this.model.item(1).getHeight()/2
         // });
         canvas.renderAll();
+    }
+
+    this.setActivation = function (type) {
+        this.activation=type;
+        this.fabricModel.getObjects()[2].setText(type);
+        canvas.renderAll();
+    }
+    this.setLayerInput = function (input) {
+
+    }
+    this.setLayerOutput = function (output) {
+
     }
 }
 
@@ -450,7 +462,7 @@ function LayerSet(){
 
     this.deleteLayer=function(position){
         if(this.layers.length ==1) return;
-        this.layers.splice(position,1);
+        this.layers.splice(position-1,1);
         for(var x=0; x<this.layers.length;x++){
             this.layers[x].setId(x+1);
         }
@@ -474,6 +486,16 @@ function LayerSet(){
         }
 
         this.fabricModel = newModel;
+    }
+
+    this.setActivation = function (layer,type) {
+        this.layers[layer-1].setActivation(type);
+    }
+    this.setLayerInput = function (layer,input) {
+
+    }
+    this.setLayerOutput = function (layer,output) {
+
     }
 
 }
@@ -520,6 +542,7 @@ function NeuralNetworks(id,pointLeft, pointTop){
     this.regularization = new Regularization();
     this.training_epoch = 1024;
     this.container=null;
+    this.ID=id;
 
     this.getContainer= function(){
         this.container = new NeuralContainer(this.layerSet.layers.length);
@@ -530,10 +553,9 @@ function NeuralNetworks(id,pointLeft, pointTop){
         left:pointLeft,
         top:pointTop
     });
-    this.fabricModel.ID=id;
+    this.fabricModel.id=this.ID;
 
     this.updateFabricModel = function(){
-
         this.layerSet.UpdateFabric();
         this.layerSet.fabricModel.set({
             left:60,
@@ -544,11 +566,60 @@ function NeuralNetworks(id,pointLeft, pointTop){
                 top:this.fabricModel.top
         });
         canvas.remove(this.fabricModel);
-        replaceModel.id= this.fabricModel.id;
         this.fabricModel = replaceModel;
+        this.fabricModel.id=this.ID;
+        this.fabricModel.on('selected',function(options){
+            currentSelectedModel=getModelById(this.id);
+            currentSelectedModel.changeOptionMenu();
+        });
         canvas.add(this.fabricModel);
         canvas.renderAll();
     }
+
+    this.fabricModel.on('selected',function(options){
+        currentSelectedModel=getModelById(this.id);
+        currentSelectedModel.changeOptionMenu();
+    });
+
+    this.changeOptionMenu =function () {
+
+        //Set Initializer
+        $('#initializer-btn').show();
+        var iniType = this.initializer.type;
+        $('#change-Initializer-current').text(iniType);
+        if(iniType=='random_uniform'){
+            $('#change-Initializer-value').hide();
+            $('#change-Initializer-value-max').show();
+            $('#change-Initializer-value-min').show();
+        }else if(iniType=='random_normal'){
+            $('#change-Initializer-value').show();
+            $('#change-Initializer-value-max').hide();
+            $('#change-Initializer-value-min').hide();
+        }
+        $('#change-Initializer-value-max-input').val(this.initializer.max);
+        $('#change-Initializer-value-min-input').val(this.initializer.min);
+        $('#change-Initializer-value-input').val(this.initializer.val);
+
+        //Set Optimizer
+        $('#optimizer-btn').show();
+        $('#change-Optimizer-current').text(this.optimizer.type);
+        $('#change-Optimizer-learningRate-input').val(this.optimizer.learningRate);
+
+        //Set Regularization
+        $('#regularization-btn').show();
+        $('#change-regularization-current').text(this.regularization.enable);
+        if(this.regularization=="false"){
+            $('#change-regularization-lambda').hide();
+        }else{
+            $('#change-regularization-lambda').show();
+        }
+        $('#change-regularization-lambda-input').val(this.regularization.lambda);
+        //Set trainingEpoch
+        $('#trainingEpoch-btn').show();
+        $('#change-trainingEpoch-input').val(this.training_epoch);
+
+    }
+
 
     this.addLayerFrontOf = function(position){
 
@@ -574,17 +645,80 @@ function NeuralNetworks(id,pointLeft, pointTop){
     this.getLayerLength = function(){
         return this.layerSet.layers.length;
     }
+
+
+    //change Initializer
+    this.changeInitializer =function (type) {
+        this.initializer.changeType(type);
+    }
+
+    //change Optimizer
+    this.changeOptimizer =function(type){
+        this.optimizer.changeOptimizer(type);
+    }
+
+    //change Regularization
+    this.setRegularizationEnable =function (can) {
+        this.regularization.changeEnable(can);
+    }
+
+    this.setLambda = function(val){
+        this.regularization.changeLambda(val);
+    }
+
+    //change training epoch
+    this.changeTrainingEpoch =function (val) {
+        this.training_epoch=val;
+    }
+    
+    this.setActivation = function (layer,type) {
+        this.layerSet.setActivation(layer,type);
+    }
+    this.setLayerInput = function (layer,input) {
+        
+    }
+    this.setLayerOutput = function (layer,output) {
+
+    }
+
 }
 
 function makeLayerOption(LayerNumber){
     var option = $('#btn-dummy').clone(true);
-    var opionLayer = $('#dummyLayer').clone(true);
+    var optionLayer = $('#dummyLayer').clone(true);
     option.attr('id','btn-layer'+LayerNumber.toString()).show();
     option.html('Layer'+LayerNumber.toString());
     option.attr('data-target','#layer'+LayerNumber.toString());
-    opionLayer.attr('id','layer'+LayerNumber.toString());
-    opionLayer.addClass('collapse');
+    optionLayer.attr('id','layer'+LayerNumber.toString());
+    optionLayer.addClass('collapse');
     $('#model-addlayer-btn').before(option);
-    $('#model-addlayer-btn').before(opionLayer);
+    $('#model-addlayer-btn').before(optionLayer);
+
+    //Event Handling
+    var btngroup= optionLayer.children().eq(0);
+    btngroup.find('a').click(function(){
+        btngroup.find('button').text($(this).text());
+        currentSelectedModel.setActivation(LayerNumber,$(this).text());
+    });
+
+    var inputDiv =optionLayer.children().eq(1).find('input');
+    inputDiv.on("change paste keyup", function() {
+        currentSelectedModel.setLayerInput($(this).val());
+    });
+
+    var outputDiv =optionLayer.children().eq(2).find('output');
+    outputDiv.on("change paste keyup", function() {
+        currentSelectedModel.setLayerOutput($(this).val());
+    });
+
+    var delBtn =optionLayer.children().eq(3);
+    delBtn.click(function(){
+        currentSelectedModel.deleteLayer(LayerNumber);
+        if(LayerNumber!=1) {
+            option.hide();
+            optionLayer.hide();
+        }
+    });
+    
 }
 
